adea631a9ba9dd1e8db0db30b589fc17b743f226
Alvaro Padilla <varoboss.ap@gmail.com>
2024-06-09 13:47:20 -0600

    Add main endpoints

--- app.rb
+++ app.rb
@@ -1,7 +1,33 @@
 require 'sinatra'
+require 'json'
 
 class AssetsTracker < Sinatra::Base
   get '/' do
-    "Tyler is a dog"
+    @assets = [
+      {
+        type: 'mouse',
+        username: 'tyler',
+        available: false,
+      },
+      {
+        type: 'pc',
+        username: nil,
+        available: true,
+      }
+    ]
+
+    erb :index
+  end
+
+  get '/assets/:id' do
+    p "show asset: #{params[:id]}"
+  end
+
+  post '/assets' do
+    p "add new asset"
+  end
+
+  post '/assets/:id' do
+    p "modify asset: #{params[:id]}"
   end
 end
