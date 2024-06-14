bf586702b690aa59621720a96380964ee17b7874
Alvaro Padilla <varoboss.ap@gmail.com>
2024-06-14 01:29:24 -0600

    Add create assets migration

---
+++ db/migrations/20240614005543_create_assets.rb
@@ -0,0 +1,19 @@
+module Migrations
+  class CreateAssets < Migration
+    def up
+      query = <<-SQL
+      CREATE TABLE assets (
+        id            INTEGER PRIMARY KEY ASC,
+        serial_number VARCHAR(80) NOT NULL,
+        type          VARCHAR(80) NOT NULL,
+        available     BOOLEAN DEFAULT 1,
+        user_id       INTEGER REFERENCES users(id) ON DELETE NO ACTION
+      );
+      SQL
+      @db.execute query
+    end
+
+    def down
+    end
+  end
+end
