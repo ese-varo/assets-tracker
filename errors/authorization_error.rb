bc88c331320d2f2e1b51970270e5eb30e8bfeb49
Alvaro Padilla <varoboss.ap@gmail.com>
2024-08-09 19:31:23 -0600

    Refactor UnauthorizeAction implementetion

--- errors/authorization_error.rb
+++ errors/authorization_error.rb
@@ -16,25 +16,34 @@ module Exceptions
     private
 
     def public_error_message
-      "You are not authorized to #{action_to_text}"
+      "You are not authorized to #{action_phrase}"
     end
 
-    def action_to_text
+    def action_phrase
       raise NotImplementedError
     end
 
     def set_log_message
-      @log_message = action == :index? ? log_list_message : log_common_message
+      @log_message = case(action)
+                     when :index? then log_index
+                     when :new? then log_new
+                     when :update?, :show?, :create?, :destroy?
+                       log_common
+                     end
     end
 
-    def log_common_message
-      "User: UNAUTHORIZED | Action: #{action_string} attempted " \
-        "on #{record.class} with ID #{record.id} " \
-        "by User with ID #{user.id} and Role #{user.role_as_string} " \
-        "(username: #{user.username}) | (401 Unauthorized)"
+    def log_base_message
+      "User: UNAUTHORIZED | User ID: #{user.id}, " \
+        "Role: #{user.role_as_string}, " \
+        "Username: #{user.username} | (401 Unauthorized)"
     end
 
-    def log_list_message
+    def log_common
+      "#{log_base_message} | Action: #{action_string} attempted " \
+        "on #{record.class} with ID #{record.id}"
+    end
+
+    def log_index
       raise NotImplementedError
     end
 
@@ -46,48 +55,62 @@ module Exceptions
   class UnauthorizedUserAction < UnauthorizedAction
     private
 
-    def action_to_text
+    def action_phrase
       case action
-      when :show?, :index?
-        'access this page'
-      when :update?
-        'update this user'
-      when :destroy?
-        'delete this user'
-      else
-        'execute this action'
+      when :show?, :index? then 'access this page'
+      when :update? then 'update this user'
+      when :destroy? then 'delete this user'
+      else 'execute this action'
       end
     end
 
-    def log_list_message
-      'User: UNAUTHORIZED | Access Users list attempted ' \
-        "by User with ID #{user.id} with Role #{user.role_as_string} " \
-        "(username: #{user.username}) | (401 Unauthorized)"
+    def log_index
+      "#{log_base_message} | Access Users list attempeted"
     end
   end
 
   class UnauthorizedAssetAction < UnauthorizedAction
     private
 
-    def action_to_text
+    def action_phrase
       case action
-      when :show?, :index?
-        'access this page'
-      when :update?
-        'update this asset'
-      when :destroy?
-        'delete this asset'
-      when :new?, :create?
-        'create an asset'
-      else
-        'execute this action'
+      when :show?, :index? then 'access this page'
+      when :update? then 'update this asset'
+      when :destroy? then 'delete this asset'
+      when :new?, :create? then 'create an asset'
+      else 'execute this action'
       end
     end
 
-    def log_list_message
-      'User: UNAUTHORIZED | Access Assets list attempted ' \
-        "by User with ID #{user.id} with Role #{user.role_as_string} " \
-        "(username: #{user.username}) | (401 Unauthorized)"
+    def set_log_message
+      if custom_actions.include? action
+        case action
+        when :show_load_csv? then log_show_load_csv
+        when :load_csv? then log_load_csv
+        end
+      else
+        super
+      end
+    end
+
+    def custom_actions
+      [:show_load_csv?, :load_csv?]
+    end
+
+    def log_new
+      "#{log_base_message} | Access Assets new form attempted"
+    end
+
+    def log_index
+      "#{log_base_message} | Access Assets list attempted"
+    end
+
+    def log_show_load_csv
+      "#{log_base_message} | Access Assets load csv form attempted"
+    end
+
+    def log_load_csv
+      "#{log_base_message} | Action: #{action_string} attempted on Assets"
     end
   end
 end
